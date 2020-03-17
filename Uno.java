a54715310c40e3c3252c3cc477736f7eef328729
gustavo <gustavo_8383@hotmail.com>
2020-03-16 20:34:48 -0600

    d

--- Uno.java
+++ Uno.java
@@ -5,5 +5,21 @@ public class Uno {
     private Tarjeta tarjeta;
     private Mazo mazo;
     private Mazo pila;
+    private ArrayList<Jugador> jugadores;
+
+    public void iniciar(){
+
+    }
+
+    public void  jugar(){
+
+    }
+
+    public void finalizar(){
+
+    }
+
+
+
     
 }
